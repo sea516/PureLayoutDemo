f32b72f72b080ec278054abfb98a391ef3a4ab00
Haiyang Jiang <jianghaiyang2010@126.com>
2015-12-10 09:24:16 +0800

    create project files

---
+++ PureLayoutDemo/PureLayoutDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PureLayoutDemo
+//
+//  Created by Haiyang Jiang on 15/12/10.
+//  Copyright © 2015年 Haiyang Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PureLayoutDemo/PureLayoutDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PureLayoutDemo
+//
+//  Created by Haiyang Jiang on 15/12/10.
+//  Copyright © 2015年 Haiyang Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
